a74866c4d96cd25e8cbf6c51b90c3c57ce7cf4b3
Harshit Sharma <hsharma1205@gmail.com>
2016-09-18 19:14:00 +0530

    Added exit condition

--- get_result.py
+++ get_result.py
@@ -13,41 +13,50 @@ def get_notified(browser, site_id, password, account_sid, auth_token, client, tw
         		'password': "//input[@name='recqIDId:j_id19']",
         		'submitButton': "//input[@name='recqIDId:j_id25']",\
         		'term': "//input[@name='scheduleForm:j_id113']" }
-
- 	browser.find_element_by_xpath(xpaths['site_id']).send_keys(site_id)
-	browser.find_element_by_xpath(xpaths['username']).send_keys(username)
-	browser.find_element_by_xpath(xpaths['password']).send_keys(password)
-	browser.find_element_by_xpath(xpaths['submitButton']).click()
-	time.sleep(10)
-	browser.find_element_by_link_text("Assignment & Grades").click();
-	time.sleep(10)
-	browser.find_element_by_id("scheduleForm:coltrm").send_keys(term)
-	browser.find_element_by_link_text("Search").click();
-	time.sleep(10)
-
-	table = browser.find_elements_by_xpath("//table[@id='scheduleForm:svres']/tbody/tr")
-	no_of_rows = len(table)
 	
-	while no_of_rows != MAX_ROWS:
-	if no_of_rows == MAX_ROWS:
-		 message = client.messages.create(
-                        body="All results are available. Bye",
-                        to= your_no,    # Replace with your phone number
-                        from_= twilio_no # Replace with your Twilio number
-                )
-		sys.exit()
- 	else:
-    		message = client.messages.create(
-    			body="Check Result!",
-          		to= your_no,    # Replace with your phone number
-           		from_= twilio_no # Replace with your Twilio number
-        	)
-#	if no_of_rows == MAX_ROWS:
-  	browser.find_element_by_link_text("logout").click();
-  	if no_of_rows == MAX_ROWS:
-  		sys.exit(1)
-	time.sleep(900)
+	no_of_rows = 0
+	current_num = 0
+	
+	while no_of_rows < MAX_ROWS:
+ 		browser.find_element_by_xpath(xpaths['site_id']).send_keys(site_id)
+		browser.find_element_by_xpath(xpaths['username']).send_keys(username)
+		browser.find_element_by_xpath(xpaths['password']).send_keys(password)
+		browser.find_element_by_xpath(xpaths['submitButton']).click()
+		time.sleep(10)
+		browser.find_element_by_link_text("Assignment & Grades").click();
+		time.sleep(10)
+		browser.find_element_by_id("scheduleForm:coltrm").send_keys(term)
+		browser.find_element_by_link_text("Search").click();
+		time.sleep(10)
 
+		table = browser.find_elements_by_xpath("//table[@id='scheduleForm:svres']/tbody/tr")
+		no_of_rows = len(table)
+	
+		if no_of_rows == MAX_ROWS:
+			 message = client.messages.create(
+                        	body="All results are available. Bye",
+                        	to= your_no,    # Replace with your phone number
+                        	from_= twilio_no # Replace with your Twilio number
+                		)	
+			sys.exit()
+ 		else:
+ 			if no_of_rows > current_num:
+    			message = client.messages.create(
+    				body="Check Result!",
+          			to= your_no,    # Replace with your phone number
+           			from_= twilio_no # Replace with your Twilio number
+        		)
+        		current_num = no_of_rows
+  		browser.find_element_by_link_text("logout").click();
+		time.sleep(900)
+		
+	if no_of_rows > current_num:
+    			message = client.messages.create(
+    				body="Check Result!",
+          			to= your_no,    # Replace with your phone number
+           			from_= twilio_no # Replace with your Twilio number
+       			)
+	
 if __name__ == "__main__":
 	browser = webdriver.PhantomJS()
 
